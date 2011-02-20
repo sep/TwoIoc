dd05a03f9a0c7d8e437b2bc70695eba17ba3556f
Jon Fuller <fullerjc@gmail.com>
2011-02-20 17:40:56 -0500

    pulled .net 3.5 things into a new project

---
+++ src/Net35/Delegates.cs
@@ -0,0 +1,13 @@
+﻿namespace Net35
+{
+    public delegate TResult Func<TResult>();
+    public delegate TResult Func<T1, TResult>(T1 param1);
+    public delegate TResult Func<T1, T2, TResult>(T1 param1, T2 param2);
+    public delegate TResult Func<T1, T2, T3, TResult>(T1 param1, T2 param2, T3 param3);
+    public delegate TResult Func<T1, T2, T3, T4, TResult>(T1 param1, T2 param2, T3 param3, T4 param4);
+
+    public delegate void Action();
+    public delegate void Action<T1, T2>(T1 param1, T2 param2);
+    public delegate void Action<T1, T2, T3>(T1 param1, T2 param2, T3 param3);
+    public delegate void Action<T1, T2, T3, T4>(T1 param1, T2 param2, T3 param3, T4 param4);
+}
---
+++ src/Net35/EnumerableExt.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Net35
+{
+    public static class EnumerableExt
+    {
+        public static IDictionary<TKey, List<TValue>> GroupBy<T, TKey, TValue>(this IEnumerable<T> target, Func<T, TKey> keySelector, Func<T, TValue> valueSelector)
+        {
+            var groups = new Dictionary<TKey, List<TValue>>();
+            foreach (var item in target)
+            {
+                var key = keySelector(item);
+                var value = valueSelector(item);
+
+                if (!groups.ContainsKey(key))
+                    groups.Add(key, new List<TValue>());
+
+                groups[key].Add(value);
+            }
+
+            return groups;
+        }
+
+        public static IDictionary<TKey, TValue> ToDictionary<T, TKey, TValue>(this IEnumerable<T> target, Func<T, TKey> keySelector, Func<T, TValue> valueSelector)
+        {
+            var dictionary = new Dictionary<TKey, TValue>();
+            foreach(var item in target)
+                dictionary.Add(keySelector(item), valueSelector(item));
+            return dictionary;
+        }
+
+        public static T IndexOrDefault<T>(this List<T> target, int index)
+        {
+            if (index < target.Count)
+                return target[index];
+            return default(T);
+        }
+
+        public static IEnumerable<T> Concat<T>(this IEnumerable<T> target, IEnumerable<T> other)
+        {
+            foreach (var item in target)
+                yield return item;
+            foreach (var item in other)
+                yield return item;
+        }
+
+        public static bool Empty<T>(this IEnumerable<T> target)
+        {
+            foreach (var _ in target)
+                return false;
+            return true;
+        }
+
+        public static bool HasOne<T>(this IEnumerable<T> target)
+        {
+            return ToList<T>(target.Take(2)).Count == 1;
+        }
+
+        public static IEnumerable<T> Cast<T>(this IEnumerable target)
+        {
+            foreach(var item in target)
+                yield return (T) item;
+        }
+
+        public static void Each<T>(this IEnumerable<T> target, Action<T> action)
+        {
+            foreach (var item in target)
+                action(item);
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> target, Func<T, bool> filter)
+        {
+            foreach (var item in target)
+                if (filter(item))
+                    yield return item;
+        }
+
+        public static IEnumerable<T> Take<T>(this IEnumerable<T> target, int numToTake)
+        {
+            int cnt = 0;
+            foreach (var item in target)
+                if (cnt++ < numToTake)
+                    yield return item;
+                else
+                    yield break;
+        }
+
+        public static IEnumerable<TResult> Select<T, TResult>(this IEnumerable<T> target, Func<T, TResult> selector)
+        {
+            foreach (var item in target)
+                yield return selector(item);
+        }
+
+        public static IEnumerable<TResult> Select<T, TResult>(this IEnumerable<T> target, Func<T, int, TResult> selector)
+        {
+            int cnt = 0;
+            foreach (var item in target)
+                yield return selector(item, cnt++);
+        }
+
+        public static IEnumerable<TResult> SelectMany<T, TResult>(this IEnumerable<T> target, Func<T, IEnumerable<TResult>> manySelector)
+        {
+            foreach (var item in target)
+                foreach (var single in manySelector(item))
+                    yield return single;
+        }
+
+        public static IEnumerable<T> Eval<T>(this IEnumerable<T> target)
+        {
+            return ToList<T>(target);
+        }
+
+        public static List<T> ToList<T>(this IEnumerable<T> target)
+        {
+            var list = new List<T>();
+            foreach (var item in target)
+                list.Add(item);
+            return list;
+        }
+
+        public static T[] ToArray<T>(this IEnumerable<T> target)
+        {
+            return ToList<T>(target).ToArray();
+        }
+
+        public static T FirstOrDefault<T>(this IEnumerable<T> target)
+        {
+            foreach (var item in target)
+                return item;
+            return default(T);
+        }
+
+        public static T FirstOrDefault<T>(this IEnumerable<T> target, Func<T, bool> filter)
+        {
+            return FirstOrDefault<T>(target.Where(filter));
+        }
+
+        public static T First<T>(this IEnumerable<T> target)
+        {
+            foreach (var item in target)
+                return item;
+            throw new InvalidOperationException("The source sequence is empty.");
+        }
+
+        public static T LastOrDefault<T>(this IEnumerable<T> target)
+        {
+            T storedItem = default(T);
+            foreach (var item in target)
+                storedItem = item;
+            return storedItem;
+        }
+
+        public static IEnumerable<T> OrderBy<T>(this IEnumerable<T> target, Func<T, int> selector)
+        {
+            var list = new List<T>(target);
+            list.Sort((x, y) => selector(x) - selector(y));
+
+            return list;
+        }
+    }
+}
