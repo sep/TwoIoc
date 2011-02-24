70e9a93b73dee95148222f5e182923c41e492b52
Jon Fuller <fullerjc@gmail.com>
2011-02-24 15:20:56 -0500

    just a little cleanup

---
+++ src/Net35/Enumerable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Net35
+{
+    public static class Enumerable
+    {
+        public static IEnumerable<int> Range(int numElements)
+        {
+            for (var i = 0; i < numElements; i++)
+                yield return i;
+        }
+    }
+}
